34b946090e64c2323cf4b150953d5c09b5864a03
MannuA19 <mannmeet.ahir@gmail.com>
2025-01-19 22:37:36 +0530

    Terraform Local code Modified

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "vpc" {}
+variable "env" {}
+variable "apps" {}
+variable "bastion_nodes" {}
+variable "db" {}
+variable "vault_token" {}
+variable "zone_id" {}
+variable "load_balancers" {}
+variable "load_balancer_type" {}
+variable "subnet_ref" {}
